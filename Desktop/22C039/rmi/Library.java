93f257e8065f68fdbd1b3c281bb934729135ba25
sampath-10 <147841965+sampath-10@users.noreply.github.com>
2024-09-27 10:33:33 +0530

    Delete Desktop directory

--- Desktop/22C039/rmi/Library.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.List;
-
-public interface Library extends Remote {
-    List<String> getAvailableBooks() throws RemoteException;
-    boolean issueBook(String title) throws RemoteException;
-    boolean returnBook(String title) throws RemoteException;
-}
